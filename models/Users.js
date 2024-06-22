8b88fff1851730dec5b7761b3f09289b476f7742
asmckeithan <annesha.mckeithan@gmail.com>
2024-06-22 16:27:17 -0400

    Add UserSchema

---
+++ models/Users.js
@@ -0,0 +1,25 @@
+//importing mongoose
+const mongoose = require('mongoose')
+
+//importing the Schema of mogoose
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }, 
+    password:{
+        type: String,
+        required: true,
+        minlength: 8
+
+    }
+})
+
+export default mongoose.model("User", usserSchema)
--- server.js
+++ server.js
@@ -3,12 +3,22 @@
 //declaring an express variable to require express
 const express = require('express');
 
+//importing mongoose
+const mongoose = require('mongoose')
+
+//inserting your driver string to connect mongoose
+mongoose.connect(
+    "mongodb+srv://asmckeithan:Q5ME2EhZba9PiA0S@firstcluster.shdprc6.mongodb.net/Capstone-Project"
+).then(() => console.log("Connected to MongoDB using Mongoose"))
 
 //create the express server inside a variable called app
 const app = express()
 
 //selecting the port you would like the server to run on.(8000) 
 const PORT = process.env.PORT || 8000;
+
+//
+
 
 app.use("/",(request,response,next) => {
     response.send("hello World")
